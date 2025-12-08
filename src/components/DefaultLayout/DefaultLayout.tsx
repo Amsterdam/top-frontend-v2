df511946459f10df0c8448c3927a42574b8ab77a
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2025-12-08 17:22:29 +0100

    WON-451-uitloggen-knop

--- src/components/DefaultLayout/DefaultLayout.tsx
+++ src/components/DefaultLayout/DefaultLayout.tsx
@@ -4,19 +4,29 @@ import { LogOutIcon, SearchIcon } from "@amsterdam/design-system-react-icons"
 import { env } from "@/config/env"
 import { useRedirectFromState } from "@/hooks/useRedirectFromState"
 import styles from "./DefaultLayout.module.css"
+import { useAuth } from "react-oidc-context"
 
 export const DefaultLayout: React.FC = () => {
   useRedirectFromState()
-
+  const auth = useAuth()
   return (
     <>
       <PageHeader
         brandName={`${env.VITE_APP_TITLE} ${env.VITE_ENVIRONMENT_SHORT}`}
         menuItems={[
-          <PageHeader.MenuLink key="2" fixed href="#" icon={SearchIcon}>
+          <PageHeader.MenuLink key="zoeken" fixed href="#" icon={SearchIcon}>
             Zoeken
           </PageHeader.MenuLink>,
-          <PageHeader.MenuLink fixed href="#" icon={LogOutIcon}>
+          <PageHeader.MenuLink
+            key="uitloggen"
+            fixed
+            href="#"
+            icon={LogOutIcon}
+            onClick={(e) => {
+              e.preventDefault()
+              auth.signoutRedirect()
+            }}
+          >
             Uitloggen
           </PageHeader.MenuLink>,
         ]}
