7a1eede14c8f79f2f67962cdb151ee9f23bd24a8
Niels Roozemond <n.roozemond@amsterdam.nl>
2026-07-23 11:47:38 +0200

    Update ChooseListPage.tsx

--- src/pages/ChooseListPage/ChooseListPage.tsx
+++ src/pages/ChooseListPage/ChooseListPage.tsx
@@ -1,22 +1,22 @@
-import { Column, Heading } from "@amsterdam/design-system-react"
+import { Column, Grid, Heading } from "@amsterdam/design-system-react"
 
-import { Tag } from "@/components"
 import { useItinerariesSummary } from "@/api/hooks/itineraries"
 import ListCard from "./ListCard"
 
 export default function ChooseListPage() {
   const [itineraries] = useItinerariesSummary()
   return (
-    <Column gap="large">
-      <Column gap="small">
-        <Heading level={2}>Mijn looplijsten</Heading>
-        <Tag name={`${itineraries?.length ?? ""} actieve lijsten`} />
-      </Column>
-      <Column gap="small">
+    <Grid paddingBottom="x-large" paddingTop="large">
+      <Grid.Cell span="all" appearance="transparent">
+        <Heading level={1}>Alle looplijsten {itineraries?.length ? `(${itineraries.length})` : ""}</Heading>
+      </Grid.Cell>
+      <Grid.Cell span="all" appearance="transparent">
+        <Column gap="large">
         {itineraries?.map((itinerary) => (
           <ListCard key={itinerary.id} itinerary={itinerary} />
         ))}
-      </Column>
-    </Column>
+        </Column>
+      </Grid.Cell>
+    </Grid>
   )
 }
